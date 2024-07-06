6244364e2799e3145c7e19bf1f7fe06478a7b371
Christin Saji <ch580848@dal.ca>
2024-07-06 15:08:42 -0300

    add GET API to list user details

--- Tutorial5/app.js
+++ Tutorial5/app.js
@@ -1,5 +1,26 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
+
+let users = [
+  { email: "abc@abc.ca", firstName: "ABC", id: "5abf6783" },
+  { email: "xyz@xyz.ca", firstName: "XYZ", id: "5abf674563" },
+];
+
+app.get("/users", (req, res) => {
+  try {
+    res.status(200).json({
+      message: "Users retrieved",
+      success: true,
+      users: users,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+      success: false,
+    });
+  }
+});
 
 app.use((req, res, next) => {
   res.status(200).json({
